977757316c0073e12253b830bb68f067c533b830
Dixon <villandixon@yahoo.com>
2020-09-26 17:14:54 +0800

    fixed netlify build warnings

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,46 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Portfolio extends Component {
   render() {
-
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-            <a target="_blank" href={projects.github} title={projects.title}>
-              <div className="link-icon"><i className="fa fa-github"></i></div>
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = "images/portfolio/" + projects.image;
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <a
+              target="_blank"
+              href={projects.github}
+              title={projects.title}
+              rel="noopener noreferrer"
+            >
+              <div className="link-icon">
+                <i className="fa fa-github"></i>
+              </div>
             </a>
-           <div className="item-wrap">
-            <a target="_blank" href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />              
-               <div className="overlay">
+            <div className="item-wrap">
+              <a
+                target="_blank"
+                href={projects.url}
+                title={projects.title}
+                rel="noopener noreferrer"
+              >
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+                <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
-      })
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>CHECK THESE SITES I AM CURRENTLY WORKING ON.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
